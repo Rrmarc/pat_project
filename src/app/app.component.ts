745aaabece72b40a07bab14e8b6b9e1414a8c149
Rashell Marcelino <rashellmarcelino@Rashells-MacBook-Air.local>
2019-04-10 14:31:51 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'patProject';
+
+  // project_name: string;
+  // project_description: string;
+  // project_image:string;
+
+
+  project:
+  project_name = 'Twdp Project';
+  project_description = 'this project was created with Ruby on Rails. It was part of NYCDA projects';
+  project_image = ':D';
+
+
+  
+
+
+
 }
